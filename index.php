3d2868f99bf39768117bc4da4f2d83d65fa44503
Wolf Devil <ahmedsafadii@gmail.com>
2015-03-31 19:33:46 +0300

    test
    test

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-<?php
-
-echo "hi";
-
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "Create UTF-8 File";
+        ?>
+    </body>
+</html>
