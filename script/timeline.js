4b27cd4e9ea1d07dc25fa2c6bcc23de59a93cd18
Wolf Devil <ahmedsafadii@gmail.com>
2015-04-02 23:14:18 +0300

    testjk
    commet

--- script/timeline.js
+++ script/timeline.js
@@ -3,9 +3,9 @@
 		// SET OPTIOSN
 		return $(this).each(function(){
 			var o = $.extend({
-				events: [1, 1, 10, 15, 900, 1800], // Event times in seconds. Callback function will notify about events
+				events: [1, 1, 10, 15, 1700], // Event times in seconds. Callback function will notify about events
 				showEvent: [true, true, true, true, true], // Should the event be shown in the bar?
-				hoverText: ["Start of game", "double", "Second Event", "Third Event", "Last event", "End of game"], // Hover text
+				hoverText: ["First event", "double", "Second Event", "Third Event", "Last event"], // Hover text
 				timeLineHeight: 10,
 				innerLineHeight: 10,
 				timeLineWidth: 1000, // width in px
@@ -17,8 +17,7 @@
 			var $currentTime = 0;
 			var $pause = false;
 			var $id = $(this).attr("id");
-			var $eventPointer = 0;
-			
+		
 			// Cache elements
 			var $container = $(this).show();
 			// Timer and button container
@@ -28,35 +27,30 @@
 			// Add Button div
 			var $button = $('<div class="button"><img data-uk-tooltip title="Play/Pasue" class="pauseplay" src="images/play.png" onClick="$(\'' + $id + '\').timeliner.pauseplay()"></div>').prependTo($timerButton);
 			// Line div
-			var $lineContainer = $('<div class="lineContainer"></div>').css({width:o.timeLineWidth+16, height:o.timeLineHeight}).appendTo($container);
+			var $lineContainer = $('<div class="lineContainer"></div>').css({width:o.timeLineWidth, height:o.timeLineHeight}).appendTo($container);
 			var $outerLineDiv = $('<div class="outerLine"></div>').css({width:o.timeLineWidth, height:o.timeLineHeight}).appendTo($lineContainer);
 			// innterLineDiv
 			var $innerLineDiv  = $('<div class="innerLine">&nbsp;</div>').css({width:0, height:o.timeLineHeight}).appendTo($outerLineDiv);
 			// Place Divs inside the innerLineDiv (length / 7)
-			var $pointDiv = $('<div class="circle"></div>').css({width:o.timeLineWidth}).appendTo($lineContainer);
+			var $pointDivs = new Array();
 			// Create clusters for events
 			var cluster = new Array(Math.floor(o.timeLineWidth / 13));
-				for(i = 0; i < o.timeLineWidth / 13+1; i++) {
+				for(i = 0; i < o.timeLineWidth / 13; i++) {
 					cluster[i] = new Array();
 				}
 			createClusters(o.events);
 			// now set the content of the pointDivs with the index inside the clusters
 			for (i = 0; i < cluster.length; i++) {
+				$pointDivs[i] = $('<div class="circle" ident="' + (i) + '">&nbsp;</div>').css({width:13, height:o.timeLineHeight}).appendTo($lineContainer);
 					if (cluster[i].length > 0) {
 						var spanText = "";
-						var sum = 0;
 						for (z = 0; z < cluster[i].length; z++) {
 							if (o.showEvent[cluster[i][z]]) {
-								sum += o.events[cluster[i][z]];
 								spanText += o.hoverText[cluster[i][z]] + '\n';
 							}
 						}
-
 						if (spanText != "") {
-							var avg = sum / cluster[i].length;
-							console.log("average" + avg);	
-							console.log("Position: " + avg/o.timeLength * o.timeLineWidth);
-							$('<div><span data-uk-tooltip title="' + spanText + '"><img class="icon" src="images/circle.png"></span></div>').css({position:"absolute", left:avg / o.timeLength * o.timeLineWidth}).appendTo($pointDiv);
+							$pointDivs[i].html('<span data-uk-tooltip title="' + spanText + '"><img class="icon" src="images/circle.png"></span>');
 						}
 					}
 			}
@@ -83,10 +77,6 @@
 			
 			function incrementTime(by) {
 				$currentTime += by;
-				while (o.events[$eventPointer] < $currentTime) {
-					the_event_callback(o.events[$eventPointer]);
-					$eventPointer++;
-				}
 				$currentTime = Math.min($currentTime, o.timeLength);
 				updateTimer();
 				if ($currentTime >= o.timeLength) {
@@ -120,10 +110,8 @@
 			
 			function createClusters(arr) {
 				border = o.timeLength * 13 / o.timeLineWidth;	
-				console.log("Border: " + border);
 				for (i = 0; i < arr.length; i++) {
 					clust = Math.floor(arr[i] / border);
-					console.log("Element at " + i + " is in " + clust);
 					cluster[clust].push(i); // store index of element in that cluster
 				}
 			}
@@ -136,13 +124,6 @@
 				} else {
 					$pause = true;
 					pauseLocal();
-				}
-			}
-			
-			// Callbacks
-			function the_event_callback(time) {
-				if(typeof event_callback == 'function'){
-					event_callback(time);
 				}
 			}
 		});
