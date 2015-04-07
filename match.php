c3f46d555b85d7629f4d1e8616a4632911d08656
Tino Fuhrmann <tino-fuhrmann@web.de>
2015-04-08 01:29:37 +0200

    Correct match data.

--- match.php
+++ match.php
@@ -1,7 +1,6 @@
 <?php
 //$content = file_get_contents("data/summoner.json");
 $content = file_get_contents("data/1136907431.json");
-echo $content;
 $content = json_decode($content, true);
 echo "<pre>";
 //var_dump($content['data']);
