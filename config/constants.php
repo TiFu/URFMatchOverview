1a1465f5271e3c6c3648683d5f7a2b73dc810807
Tino Fuhrmann <tino-fuhrmann@web.de>
2015-04-08 01:58:00 +0200

    Changed average match data table

--- config/constants.php
+++ config/constants.php
@@ -1,7 +1,7 @@
 <?php
 define("CHAMP_TABLE", "champs");
 define("ITEM_TABLE", "items");
-define("AVERAGE_TABLE", "averages");
+define("AVERAGE_TABLE", "analyzedgames");
 define("SUMMONERS_TABLE", "summoners");
 define("MATCH_PATH", "matches/");
 define("ID_PATH", "ids/");
