60a187dbb6514ca7a2fa5ad805a2b2a0c5033181
Tino Fuhrmann <tino-fuhrmann@web.de>
2015-04-08 01:23:35 +0200

    Replaced not merged file in data. Dump this file in match because Kassa buying 2 (!) Rabadons

--- match.php
+++ match.php
@@ -1,6 +1,7 @@
 <?php
 //$content = file_get_contents("data/summoner.json");
-$content = file_get_contents("data/2040109985.json");
+$content = file_get_contents("data/1136907431.json");
+echo $content;
 $content = json_decode($content, true);
 echo "<pre>";
 //var_dump($content['data']);
